25f872704e5bff20b354b1a50e76b146627ba694
Marco <100785809+Marco5dev@users.noreply.github.com>
2022-09-03 18:57:08 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+function openPage(pageName, elmnt, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpen").click();
+
+if(window.maxWidth == 1023){
+document.getElementsByClassName("main-img").style.display = "block";
+document.getElementsByClassName("main-img").style.float = "right";
+document.getElementsByClassName("main-img").style.width =  "300px";
+}
+
